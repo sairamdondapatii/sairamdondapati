05e73a89d6ebe3554d2024730abc4f1a40918616
sairamdondapatii <sairamdondapatii@gmail.com>
2024-01-31 14:32:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggle(){
+    let icon=document.getElementById('icon');
+    let headerlinks=document.getElementById('header-links');
+    headerlinks.classList.toggle('active');
+    icon.classList.toggle('active');
+
+}
+
+
+function myfunction(){
+    window.alert("submitted sucessfully");
+}
+
+
+
+
+
