40636d40d4aee701c30d7b294a6f4ca6bcb0ab9d
sairamdondapatii <sairamdondapatii@gmail.com>
2024-02-01 14:58:16 +0530

    updated

--- script.js
+++ script.js
@@ -7,17 +7,15 @@ function toggle(){
 }
 
 
-const li= document.querySelectorAll('li');
 
-for(let i=0; i<li.length; i++){
-    li[i].addEventListener('click',()=>{
+const links=document.querySelectorAll('.header-sm-links')
+for(let i=0; i<links.length; i++){
+    links[i].addEventListener('click', () =>{
         let headerlinks=document.getElementById('header-links');
         headerlinks.classList.remove('active');
         icon.classList.remove('active');
-
     })
 }
-
 
 
 
