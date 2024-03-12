5a89602111f7a3942949f39b6768c8e0bd1a7f1a
sairamdondapatii <sairamdondapatii@gmail.com>
2024-03-12 08:55:29 +0530

    updated

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 function toggle(){
-    let icon=document.getElementById('icon');
     let headerlinks=document.getElementById('header-links');
-    let fa=document.getElementById('fa');
     headerlinks.classList.toggle('active');
     icontoggle()
 
 }
 
 function icontoggle(){
-    if(fa.classList=='fa fa-bars'){
-        fa.classList.remove('fa-bars');
-        fa.classList.add('fa-close');
+    let menu=document.getElementById('menu');
+    if(menu.innerText=='menu'){
+        menu.innerText='close';
     }else{
-        fa.classList.remove('fa-close');
-        fa.classList.add('fa-bars');
+        menu.innerText='menu';
     }
 }
-
 const links=document.querySelectorAll('.header-sm-links')
 for(let i=0; i<links.length; i++){
     links[i].addEventListener('click', () =>{
@@ -25,12 +21,6 @@ for(let i=0; i<links.length; i++){
         icontoggle();
     })
 }
-
-/*let form=document.getElementById('form');
-form.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    alert('Submitted sucessfully')
-})*/
 
 
 
